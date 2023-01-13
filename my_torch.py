1058508272b638ae537b94aa2d506913399158ba
TadPal <tadeas.black@gmail.com>
2023-01-13 09:31:42 +0100

    still not working

--- my_torch.py
+++ my_torch.py
@@ -19,10 +19,10 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 25 * 25)
+        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.softmax(self.fc3(x))
         return x
 
 net = Net()
@@ -32,25 +32,51 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 # Load the data
-transform = transforms.Compose([transforms.Resize((400, 400)),
+transform = transforms.Compose([transforms.Resize((254, 254)),
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 banana_dataset = torchvision.datasets.ImageFolder(root='banana_data', transform=transform)
-dataloader = torch.utils.data.DataLoader(banana_dataset, batch_size=32, shuffle=True)
+dataloader = torch.utils.data.DataLoader(banana_dataset, batch_size=2, shuffle=True, drop_last=True)
 
 # Train the model
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(10):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(dataloader, 0):
         inputs, labels = data
+        
+        # convert the labels to the correct format
+        labels = labels.long() 
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
         outputs = net(inputs)
+        
+        # check the size and shape of the input tensor
+        print(f'inputs size: {inputs.size()}')
+        print(f'inputs shape: {inputs.shape}')
+
+        # check the size and shape of the labels tensor
+        print(f'labels size: {labels.size()}')
+        print(f'labels shape: {labels.shape}')
+
+        # check the size and shape of the output tensor
+        print(f'outputs size: {outputs.size()}')
+        print(f'outputs shape: {outputs.shape}')
+
+        # check the number of elements in the input tensor
+        print(f'inputs numel: {inputs.numel()}')
+
+        # check the number of elements in the labels tensor
+        print(f'labels numel: {labels.numel()}')
+
+        # check the number of elements in the output tensor
+        print(f'outputs numel: {outputs.numel()}')
+        
+        labels = labels.unsqueeze(1)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -64,5 +90,4 @@ print('Finished Training')
 
 torch.save({"epoch" : epoch, 
             "model_state_dict" : net.state_dict(),
-            "optimizer_state_dict" : optimizer.state_dict(),
-            "loss" : loss}, 'model/model.pth')
+            "optimizer_state_dict" : optimizer.state_dict()}, 'model/model.pth')
