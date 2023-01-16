ee969240dfe873f5c0da0d914e0c7d82910699fd
TadPal <tadeas.black@gmail.com>
2023-01-16 20:15:17 +0100

    Folders

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import base64logic as el
-import CaesarsCipherLogic as cl
-import RSALogic as rsa
+import EncodeLogics.base64logic as el
+import EncodeLogics.CaesarsCipherLogic as cl
+import EncodeLogics.RSALogic as rsa
 
-import EncoderDecoderView as ev
-import EncoderDecoderRSAView as ersa
+import TkinterView.EncoderDecoderView as ev
+import TkinterView.EncoderDecoderRSAView as ersa
 
 if __name__ == '__main__':
     logic = rsa.Logic()
